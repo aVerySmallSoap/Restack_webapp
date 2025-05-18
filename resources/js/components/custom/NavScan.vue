6f38ca234189cb03dc5aef98c451782ee464ba5c
Lirys <iamyourmarkieboo@gmail.com>
2025-05-19 01:11:52 +0800

    Added a custom sidebar component for non collapsable groups

---
+++ resources/js/components/custom/NavScan.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import {
+    SidebarGroup, SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from '@/components/ui/sidebar';
+import { type LucideIcon } from 'lucide-vue-next'
+
+defineProps<{
+  items: {
+    title: string
+    url: string
+    icon?: LucideIcon
+  }[]
+}>()
+</script>
+
+<template>
+  <SidebarGroup>
+      <SidebarGroupLabel>Scanning</SidebarGroupLabel>
+      <SidebarGroupContent>
+          <SidebarMenu>
+              <SidebarMenuItem v-for="item in items" :key="item.title">
+                  <SidebarMenuButton as-child>
+                      <a :href="item.url">
+                          <component :is="item.icon"/>
+                          <span>{{item.title}}</span>
+                      </a>
+                  </SidebarMenuButton>
+              </SidebarMenuItem>
+          </SidebarMenu>
+      </SidebarGroupContent>
+  </SidebarGroup>
+</template>
