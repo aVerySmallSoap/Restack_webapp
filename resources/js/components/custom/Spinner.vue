cd9f8ddf1d35a01b08be9a9cad14bce4cab7942e
dongjilo <angelomariano2004@gmail.com>
2025-05-21 04:37:33 +0800

    added spinner and accordion components

---
+++ resources/js/components/custom/Spinner.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex flex-col items-center justify-center">
+    <svg
+      class="animate-spin h-10 w-10 text-green-400"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <circle
+        class="opacity-25"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        stroke-width="4"
+      ></circle>
+      <path
+        class="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+      ></path>
+    </svg>
+    <span v-if="message" class="mt-2 text-white">{{ message }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ message?: string }>()
+</script>
