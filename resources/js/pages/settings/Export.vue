e6387974eae7cfb501fa7476a5a8e821c997c39a
dongjilo <angelomariano2004@gmail.com>
2025-05-26 21:26:59 +0800

    Removed unused import

--- resources/js/pages/settings/Export.vue
+++ resources/js/pages/settings/Export.vue
@@ -41,7 +41,6 @@ import { Label } from '@/components/ui/label'
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
 import { Button } from '@/components/ui/button'
 import Navigation from '@/components/custom/Navigation.vue'
-import { Separator } from '@/components/ui/separator';
 
 const exportFormat = ref<'csv' | 'json'>('csv')
 
